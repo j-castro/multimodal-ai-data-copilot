9aac264812d4624f2cb2593f4fb6c89ab5d2a42a
Jorge <pctreepkfloyd@gmail.com>
2026-04-04 22:25:49 -0600

    Adding token information

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import tiktoken
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 print(f'API Key: {api_key}')
 
 client = OpenAI()
+
+enc = tiktoken.encoding_for_model("gpt-4o-mini")
+tokens = len(enc.encode("Hello"))
+
+print(f'Tokens used: {tokens}')
 
 response = client.chat.completions.create(
     model="gpt-4o-mini",
@@ -15,3 +21,5 @@ response = client.chat.completions.create(
 )
 
 print(response.choices[0].message.content)
+
+
